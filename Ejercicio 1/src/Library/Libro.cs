e278bdb5c9235920490e6ae918113a93a4b88daf
NicorRodriguez <nicolas@nicolass-MacBook-Air-565.local>
2022-08-29 20:13:08 -0300

    Added comments about what is wrong in the code.

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,6 +8,10 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
+
+        /*
+        Las propiedades LibrarySector & LibraryShelve no deberian estar en la clase Book sino que deberian pertenecer a una clase llamada biblioteca.
+        */
         public string LibrarySector { get ; set; }
         public string LibraryShelve { get ; set; }
 
@@ -17,7 +21,9 @@ namespace SRP
             this.Author = author;
             this.Code = code;
         }
-
+        /*
+        El metodo ShelveBook no deberia estar en la clase Book sino que deberia pertenecer a una clase llamada biblioteca.
+        */
         public void ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
